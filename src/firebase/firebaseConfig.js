d0029fb8fbd7c834953885c725e8aaf7cd66154b
TerraNova13365356 <161228146+TerraNova13365356@users.noreply.github.com>
2025-10-04 22:26:34 +0530

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,16 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, get, set, onValue } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDwUtdCi1Wz-meVdKagFZ2OXLyxAUsUiAc",
-    authDomain: "code-box-ee11f.firebaseapp.com",
-    databaseURL: "https://code-box-ee11f-default-rtdb.firebaseio.com",
+    apiKey: "API KEY",
+    authDomain: "AUTH DOMAIN",
+    databaseURL: "DB URL",
     projectId: "code-box-ee11f",
-    storageBucket: "code-box-ee11f.appspot.com",
-    messagingSenderId: "956121181787",
-    appId: "1:956121181787:web:7e2126bd435a4ad871597e"
+    storageBucket: "STORAGE BUCKET",
+    messagingSenderId: "MESSAGE ID",
+    appId: "APP ID"
   };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
 export { db, ref, get, set, onValue };
+
